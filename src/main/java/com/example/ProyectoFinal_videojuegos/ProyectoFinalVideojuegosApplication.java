ec57bf6947d7c857fdf4f83dad8df17dcc58de64
JuanDa <jd.cartagena21@ciaf.edu.co>
2025-11-22 15:39:51 -0500

    Proyecto base epicardo

---
+++ src/main/java/com/example/ProyectoFinal_videojuegos/ProyectoFinalVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoFinal_videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalVideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalVideojuegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoFinal_videojuegos/ProyectoFinalVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoFinal_videojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalVideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
